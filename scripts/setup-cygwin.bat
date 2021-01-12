8f5b0cb123ac88d69af86cd89fae7cf603bab04d
Reece Russell <reece@prominentmedia.com>
2021-01-12 21:57:55 +0000

    added download step

--- scripts/setup-cygwin.bat
+++ scripts/setup-cygwin.bat
@@ -1,16 +1,12 @@
 @ECHO OFF
-REM
 
-SETLOCAL
-REM
-cd %HOMEPATH%\Downloads
+REM -- Downloading cygwin
+curl https://www.cygwin.com/setup-x86_64.exe -O setup-x86_64.exe
 
-SET SITE=http://cygwin.mirrors.pair.com/
 SET LOCALDIR=%CD%
 SET ROOTDIR=C:/cygwin
-
 SET PACKAGES=bash,bash-completion,openssh,rxvt,python,git,wget,curl,vim,gcc-core,make
 
-REM
+REM -- Running cygwin setup
 echo Installing
 setup-x86.exe --disable-buggy-antivirus -q -s %SITE% -l "%LOCALDIR%" -R "%ROOTDIR%" -P "%PACKAGES%"
