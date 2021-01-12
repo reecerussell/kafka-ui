029301e9c99b5805d26fdad5a89b8f9a06d18981
Reece Russell <reece@prominentmedia.com>
2021-01-12 22:26:33 +0000

    fixed setup

--- scripts/setup-cygwin.bat
+++ scripts/setup-cygwin.bat
@@ -3,10 +3,10 @@
 REM -- Downloading cygwin
 curl "https://www.cygwin.com/setup-x86_64.exe" -O setup-x86_64.exe
 
-SET LOCALDIR=%CD%
 SET ROOTDIR=C:/cygwin
 SET PACKAGES=bash,bash-completion,openssh,rxvt,python,git,wget,curl,vim,gcc-core,make
+SET SITE=https://cygwin.mirror.uk.sargasso.net
 
 REM -- Running cygwin setup
 echo Installing
-setup-x86_64.exe -R "%ROOTDIR%" -P "%PACKAGES%"
+setup-x86_64.exe -q -s "%SITE%" -R "%ROOTDIR%" -P "%PACKAGES%"
